854903ce5a2e2edbde09ec823bc6aa380fbdcd44
A-Aizawa <ednats18@gmail.com>
2026-05-02 16:13:04 +0900

    Remove ricty

---
+++ scripts/install-fonts.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# Nerd Fontsのインストール
+echo "Installing Nerd Fonts..."
+brew install --cask font-hack-nerd-font
+
+echo "Font installation complete!"
--- scripts/install-ricty-with-powerline.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-brew tap sanemat/font
-brew install ricty --with-powerline
-
-# copy font
-cp -f /opt/homebrew/opt/ricty/share/fonts/Ricty*.ttf ~/Library/Fonts/
-# clear font cache
-fc-cache -vf
